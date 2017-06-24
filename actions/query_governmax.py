d701350795083a64fef1034438f431ae792967e7
Charles Lane <charles@gooddonegreat.com>
2017-06-24 14:40:55 -0400

    2012

---
+++ actions/query_governmax.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..')))
+from lib.CharlestonCounty import WebParser
+
+
+governmax_api_key = 'E4D2DBDC03F14F9792D58D89BFB30CC7'
+
+Governmax = WebParser(governmax_api_key)
+Governmax.writeAndQuery('2013', exportPath='../data/governmax-2013.csv')
