c8a3e36c7c8676a848839908f6f81f5a7c0dd45b
Charles Lane <charles@gooddonegreat.com>
2017-06-26 17:12:06 -0400

    commital

--- tests/webparser_tests.py
+++ tests/webparser_tests.py
@@ -3,9 +3,8 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join('..')))
 from lib.CharlestonCounty import WebParser
 from lib.zillowAPI import ZillowAPI
-from lib.PropertyData import getPropertyInfo
-property_pin = '3500600166'
-governmax_api_key = '4CC385361D944882A296CDFDBBF886BD'
+property_pin = '4581301045'
+governmax_api_key = 'FEF8EC5AED114482874581334CADB4C8'
 
 
 class TestStringMethods(unittest.TestCase):
@@ -13,25 +12,39 @@ class TestStringMethods(unittest.TestCase):
     def test_connection_county(self):
         """This is to test the connection with governmax."""
         web_parser = WebParser(governmax_api_key)
-        data = web_parser.getDataByPin(property_pin)
-        print data
-        self.assertEqual(data['address'], '13 TRADD ST, CHARLESTON')
-
-    # def test_zillow_results(self):
-    #     """This calls zillow with the address supplied from governmax."""
-    #     web_parser = WebParser(property_pin)
-    #     web_parser.getDataByPin(governmax_api_key)
-    #     zillow_address = web_parser.formatAddressForZillow()
-    #     zillow_api = ZillowAPI(zillow_address)
-    #     zillow_results = zillow_api.queryZillow(zillow_address)
-    #     self.assertEqual(zillow_results['yearBuilt'], '1778')
-    #
-    # def test_get_property_data(self):
-    #     """This calls governmax and zillow."""
-    #     print 'getPropertyInfo'
-    #     results = getPropertyInfo(governmax_api_key, property_pin)
-    #     print results
-    #     self.assertEqual(results['yearBuilt'], '1778')
+        web_parser.getSoup(property_pin)
+        overview = web_parser.getOverview()
+        self.assertEqual(overview['Parcel Address'], '13 TRADD ST, CHARLESTON')
+    def test_current_parcel_info(self):
+        """"This checks currenc parcel info"""
+        web_parser = WebParser(governmax_api_key)
+        web_parser.getSoup(property_pin)
+        data = web_parser.getCurrentParcelInfo()
+        self.assertEqual(data['Owner'], 'MARCUS ARNOLD L TRUST MARCUS BARBARA C TRUST')
+    def test_historic_information(self):
+        """"This checks historic parcel info"""
+        web_parser = WebParser(governmax_api_key)
+        web_parser.getSoup(property_pin)
+        data = web_parser.getHistoricInformation()
+        self.assertEqual(data[0]['Land'], '$779,000')
+    def test_sales_disclosure(self):
+        """"This checks historic parcel info"""
+        web_parser = WebParser(governmax_api_key)
+        web_parser.getSoup(property_pin)
+        data = web_parser.getSalesDisclosure()
+        self.assertEqual(data[0]['Date'], '12/23/2013')
+    def test_get_improvements(self):
+        """"This checks historic parcel info"""
+        web_parser = WebParser(governmax_api_key)
+        web_parser.getSoup(property_pin)
+        data = web_parser.getImprovements()
+        self.assertEqual(data[0]['Finished Sq. Ft.'], '2,291')
+    def test_aggregate(self):
+        web_parser = WebParser(governmax_api_key)
+        web_parser.getSoup(property_pin)
+        data = web_parser.aggregateData('2013')
+        print (data, 'data')
+        self.assertEqual(data['lowestSalesValue'], 5.0)
 
 
 if __name__ == '__main__':
